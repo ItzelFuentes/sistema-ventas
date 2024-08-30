53c9b7a50a5c4e219f8845cd57c9c75f1a87294c
Alessandra Fuentes <itzelfuentescabrera@gmail.com>
2024-08-30 14:36:46 -0600

    Cambios en rutas env

--- sistema-ventas-app/dist/sistema-ventas-app/browser/chunk-DXUOWDJ6.js
+++
@@ -1,1 +0,0 @@
-var t={API_URL:"http://18.119.98.182:3000/"};export{t as a};
--- sistema-ventas-app/src/environments/environment.development.ts
+++ sistema-ventas-app/src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL: "http://18.119.98.182:3000/"
+    API_URL: "http://18.119.98.182:3000/api"
 };
